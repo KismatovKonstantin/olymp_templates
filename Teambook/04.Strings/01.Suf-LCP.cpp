63a354d06608e3134eec2db6f1dfd4de987be3a1
ralPeople <84584415+ralPeople@users.noreply.github.com>
2025-11-06 21:59:14 +0400

    Update 01.Suf-LCP.cpp
    Новая версия от Вани

--- Teambook/04.Strings/01.Suf-LCP.cpp
+++ Teambook/04.Strings/01.Suf-LCP.cpp
@@ -1,64 +1,110 @@
-string s;
-vi p, c, rc;
-int x = 1;
-int gett(int a, int x) {
-    if (a > x) return a - x;
-    return sz(s) - 1 - (x - a) % (sz(s) - 1);
-}
-int gett2(int a, int x) {
-    if (a + x < sz(s))
-        return a + x;
-    return 1 + (a + x) % sz(s);
-}
-bool comp1(int a, int b) {
-    return (s[a] < s[b]);
-}
-int n, S;
-vvi e;
-void Sort(vector<int> &p) {
-    fi(1, n) {
-        int t = gett(p[i], x);
-        e[rc[t]].pb(t);
+struct Suff_arr{
+  char LW = 3;
+  int A = 256;
+  string s0;
+  string s1;
+  vi arr;
+  vi cost;
+  vi rcost;
+  vector<vector<pair<pair<int,int>,int > > > e1;
+  vector<vector<pair<pair<int,int>,int > > > e2;
+  vector<pair<pair<int,int>,int > > t;
+  void Sort(vector<pair<pair<int,int>,int > >& t){
+    fi(0,sz(t) - 1){
+      e1[t[i].first.second].pb(t[i]);
     }
-    p.clear();
-    p.pb(0);
-    fi(0, n) {
-        fz(e[i]) {
-            p.pb(z);
-        }
-        e[i].clear();
+    fi(0,sz(e1) - 1){
+      fx(e1[i]){
+        e2[x.first.first].pb(x);
+      }
+      e1[i].clear();
     }
-}
-void init() {
-    n = sz(s) + 1;
-    s = "#" + s + (char)31;
-    c = vi(n + 1);
-    rc = vi(n + 1);
-    fi(0, n) p.pb(i);
-    sort(Allf(p), comp1);
-    c[1] = 0;
-    fi(2, n) {
-        if (s[p[i]] == s[p[i - 1]]) c[i] = c[i - 1];
-        else c[i] = c[i - 1] + 1;
+    t.clear();
+    fi(0,sz(e2) - 1){
+      fx(e2[i]){
+        t.pb(x);
+      }
+      e2[i].clear();
     }
-    fi(1, n) {
-        rc[p[i]] = c[i];
+    
+  }
+  void init(string s){
+    s0 = s;
+    s1 = s;
+    s1 += LW;
+    fi(0,sz(s0) - 1){
+      if( (sz(s1) & (sz(s1) - 1)) == 0)break;
+      s1 += s0[i];
     }
-    e = vvi(n + 1);
-    x = 1;
-    while (x < n) {
-        Sort(p);
-        c[1] = 0;
-        fi(2, n) {
-            if (rc[p[i]] == rc[p[i - 1]] && rc[gett2(p[i], x)] == rc[gett2(p[i - 1], x)]) {
-                c[i] = c[i - 1];
-            } else {
-                c[i] = c[i - 1] + 1;
-            }
-        }
-        fi(1, n) {
-            rc[p[i]] = c[i];
-        }
-        x *= 2;
+    vvi e(A + 1);
+    fi(0,sz(s1) -1){
+      e[s1[i]].pb(i);
+    }  
+    fi(0,A){
+      fx(e[i]){
+        arr.pb(x);
+      }
     }
-}
+    cost = vi(sz(arr));
+    rcost = vi(sz(arr));
+    fi(1,sz(arr) - 1){
+      cost[i] = cost[i-1];
+      if(s1[arr[i]] != s1[arr[i-1]]){
+        cost[i]++;
+      }
+    }  
+    int SIZ = 2;
+    e1.resize(sz(arr));
+    e2.resize(sz(arr));
+    while(SIZ <= sz(arr)){
+      fi(0,sz(arr) - 1){
+        rcost[arr[i]] = cost[i];
+      }  
+      t.clear();
+      fi(0,sz(arr) - 1){
+        t.pb( mp( mp(rcost[arr[i]], rcost[ (arr[i] + SIZ / 2)%sz(arr)]), arr[i]) );
+      }
+      Sort(t);
+      cost[0] = 0;
+      arr[0] = t[0].second;
+      fi(1,sz(arr) - 1){
+        arr[i] = t[i].second;
+        cost[i] = cost[i- 1];
+        if(t[i].first != t[i - 1].first)cost[i]++;
+      }
+      SIZ *= 2;
+    }
+    
+    vi arr2;
+    fx(arr){
+      if(x < sz(s0)){
+        arr2.pb(x);
+      }
+    }
+    arr = arr2;
+  }
+  vi LCP;
+  void init_lcp(){
+    vi rarr(sz(arr));
+    fi(0,sz(arr) - 1){
+      rarr[arr[i]] = i;
+    }
+    LCP = vi(sz(arr));
+    int len = 0;
+    fi(0,sz(arr) - 1){
+      if(rarr[i] == sz(arr) - 1){
+        len = 0;
+        continue;
+      }
+      len = max(0,len - 1);
+      int x = i;
+      int y = arr[rarr[i] + 1];
+      while(x + len < sz(s0) && y + len < sz(s0) && s0[x + len] == s0[y + len]){
+        len++;
+      }
+      LCP[rarr[i]] = len;
+      
+    }
+    
+  }
+};
